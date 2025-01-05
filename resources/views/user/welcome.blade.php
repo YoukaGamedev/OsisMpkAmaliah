811cf9e7bf93c6991e34bc4d57ddf36f97cff5a6
niswatul <nswaawaa@gmail.com>
2025-01-05 16:03:03 +0700

    tes

--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -5,6 +5,7 @@
 </div>
     <div class="welcome-message">
         Selamat Datang di Aplikasi OSIS dan MPK
+        <p>halo, ini adalah apk osis yang dibuat sama kelas 11 pplg 3</p>
     </div>
     <div class="container">
         <a style="text-decoration: none;" href="{{ ('/user/home') }}"><div class="box highlight pink">
