ee3be4f4d6950ad934a9cc3b2d2b010ddb3f64ec
YoukaGamedev <rizzcool13@gmail.com>
2025-12-05 15:59:42 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,24 @@ class DatabaseSeeder extends Seeder
         $this->call([
             XII_PPLG_3::class,
             XII_PPLG_2::class,
+            XII_PPLG_1::class,
+            XII_TJKT_1::class,
+            XII_TJKT_2::class,
+            XII_AN::class,
+            XI_PPLG_3::class,
+            XI_PPLG_2::class,
+            XI_PPLG_1::class,
+            XI_TJKT_1::class,
+            XI_TJKT_2::class,
+            XI_TJKT_3::class,
+            XI_AN::class,
+            X_PPLG_1::class,
+            X_PPLG_2::class,
+            X_PPLG_3::class,
+            X_TJKT_1::class,
+            X_TJKT_2::class,
+            X_AN::class,
+            X_DKV::class,
         ]);
     }
 }
